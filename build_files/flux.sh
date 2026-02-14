d3f2c28aa2cd3a489726310e960b0e006a0ab3e1
lina-bh <36717206+lina-bh@users.noreply.github.com>
2026-02-14 00:30:25 +0000

    use flux container image

--- build_files/flux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euxo pipefail
-: "${FLUX_RELEASE:=2.7.5}"
-arch=
-case $(uname -m) in
-  x86_64) arch=amd64;;
-  aarch64) arch=arm64;;
-esac
-curl -fL "https://github.com/fluxcd/flux2/releases/download/v${FLUX_RELEASE}/flux_${FLUX_RELEASE}_linux_${arch}.tar.gz" | tar -xzC /
